b47ce02200396f794e22550ece8e9c49150b03b5
CharonShen <61600619@qq.com>
2020-05-03 20:21:39 +0800

    Update:ProofOfWork.go

--- Basic-Prototype/BLC/ProofOfWork.go
+++ Basic-Prototype/BLC/ProofOfWork.go
@@ -1,6 +1,11 @@
 package BLC
 
-import "math/big"
+import (
+	"bytes"
+	"crypto/sha256"
+	"fmt"
+	"math/big"
+)
 
 const targetBit = 16
 
@@ -9,9 +14,50 @@ type ProofOfWork struct {
 	target *big.Int
 }
 
-func (pow *ProofOfWork) Run() ([]byte, int64) {
+func (pow *ProofOfWork) prepareData(nonce int64) []byte {
+	data := bytes.Join(
+		[][]byte{
+			IntToHex(pow.Block.Height),
+			pow.Block.PreBlockHash,
+			pow.Block.Data,
+			IntToHex(pow.Block.Timestamp),
+			IntToHex(nonce),
+		},
+		[]byte{},
+	)
+	return data
+}
 
-	return nil, 0
+func (pow *ProofOfWork) IsValid() bool {
+	var hashInt big.Int
+	hashInt.SetBytes(pow.Block.Hash)
+
+	if pow.target.Cmp(&hashInt) == 1 {
+		return true
+	}
+	return false
+}
+
+func (pow *ProofOfWork) Run() ([]byte, int64) {
+	var nonce int64 = 0
+	var hashInt big.Int
+	var hash [32]byte
+
+	for {
+		dataBytes := pow.prepareData(nonce)
+
+		hash = sha256.Sum256(dataBytes)
+		fmt.Printf("\r%x", hash)
+
+		hashInt.SetBytes(hash[:])
+		//判断hashInt是否小于Block里面的target
+		if pow.target.Cmp(&hashInt) == 1 {
+			break
+		}
+
+		nonce = nonce + 1
+	}
+	return hash[:], nonce
 }
 
 func NewProofOfWork(block *Block) *ProofOfWork {
--- Basic-Prototype/main.go
+++ Basic-Prototype/main.go
@@ -6,16 +6,11 @@ import (
 )
 
 func main() {
-	BlockChain := BLC.CreateBlockChainWithGensisBlock()
+	block := BLC.NewBlock("Test", 1, []byte{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0})
+	fmt.Printf("%d\n", block.Nonce)
+	fmt.Printf("%x\n", block.Hash)
 
-	BlockChain.AddBlockToBlockChain("Send 100RMB To sxz", BlockChain.Blocks[len(BlockChain.Blocks)-1].Height+1, BlockChain.Blocks[len(BlockChain.Blocks)-1].Hash)
+	proofOfWork := BLC.NewProofOfWork(block)
 
-	BlockChain.AddBlockToBlockChain("Send 200RMB To zyy", BlockChain.Blocks[len(BlockChain.Blocks)-1].Height+1, BlockChain.Blocks[len(BlockChain.Blocks)-1].Hash)
-
-	BlockChain.AddBlockToBlockChain("Send 300RMB To cnm", BlockChain.Blocks[len(BlockChain.Blocks)-1].Height+1, BlockChain.Blocks[len(BlockChain.Blocks)-1].Hash)
-
-	BlockChain.AddBlockToBlockChain("Send 50RMB To abc", BlockChain.Blocks[len(BlockChain.Blocks)-1].Height+1, BlockChain.Blocks[len(BlockChain.Blocks)-1].Hash)
-
-	fmt.Println(BlockChain)
-	fmt.Println(BlockChain.Blocks)
+	fmt.Printf("%v", proofOfWork.IsValid())
 }
